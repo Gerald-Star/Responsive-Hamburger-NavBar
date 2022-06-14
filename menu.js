64727d4b76a3e08dc2bb3c0c90f277234001f318
Gerald-Star <ukwuomagerald@gmail.com>
2022-06-14 02:35:56 +0200

    second commit dugging javascript

--- menu.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector('hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelector('.nav-links');
-
-/* add event listener */
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-}
-)
